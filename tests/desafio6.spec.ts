cea0045a3302af22e5f2c34287e89ad0d5b0bbd3
Mauro Navetta <mauronavet@gmail.com>
2026-06-24 19:06:56 -0300

    Día 6: - Seleccionar un usuario para Editar

---
+++ tests/desafio6.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test'
+
+test('Select specific user for edition', async ({ page }) => {
+
+  await page.goto('https://opensource-demo.orangehrmlive.com/')
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+  await page.getByRole('button', { name: 'Login' }).click()
+
+  await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+
+  await page.getByRole('link', { name: 'Admin' }).click()
+
+  await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('User Management').click()
+  await page.getByRole('menuitem', { name: 'Users' }).click()
+
+  const rows = page.getByRole('table').getByRole('row');
+  await rows.first().waitFor({ state: 'visible' });
+  const rowCount = await rows.count();
+
+  const randomIndex = Math.floor(Math.random() * rowCount);
+  
+  const userForEdition = await rows.nth(randomIndex).getByRole('cell').nth(1).innerText();
+  const pencilToEdit = page
+    .getByRole('table')
+    .getByRole('row')
+    .filter({ hasText: userForEdition })
+    .locator('button')
+    .filter({ has: page.locator('i.bi-pencil-fill') })
+
+await pencilToEdit.click()
+
+const currentUsername = await page.locator("//label[contains(., 'Username')]/parent::div/following-sibling::div/input")
+  .inputValue()
+
+expect(page.locator("//label[contains(., 'Username')]/parent::div/following-sibling::div/input"))
+  .toHaveValue(currentUsername)
+})
